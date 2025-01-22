756bca33e0d509bac1d941f46dd8ba4753ebf148
hansekontor <schulz.henrik@pm.me>
2025-01-22 10:56:33 +0100

    add multiplier

--- src/components/Common/RandomNumbers.js
+++ src/components/Common/RandomNumbers.js
@@ -154,6 +154,28 @@ RandomNumbers.propTypes = {
 	background: PropTypes.string 
 }
 
+const Multiplier = styled.div`
+	background-color: yellow;
+	height: 50px;
+	width: 50px;
+	border-radius: 30px;
+	position: absolute;
+	right: 0;
+	z-index: 10;
+	font-size: 20px;
+	font-weight: 600;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const playerWinningsTier = [
+	{ threshold: 0, multiplier: 16},
+	{ threshold: 4, multiplier: 8},
+	{ threshold: 6, multiplier: 4},
+	{ threshold: 11, multiplier: 2},
+	{ threshold: 35, multiplier: 1},
+];
 
 export const ResultingNumbers = ({
     numberArray, 
@@ -164,6 +186,8 @@ export const ResultingNumbers = ({
     const [showSecondNumber, setShowSecondNumber] = useState(false);
     const [showThirdNumber, setShowThirdNumber] = useState(false);
     const [showFourthNumber, setShowFourthNumber] = useState(false);
+	const [multiplier, setMultiplier] = useState(16);
+
     
 	const indexMap = new Map();
 	for (let i = 0; i < numberArray.length; i++) {
@@ -175,9 +199,11 @@ export const ResultingNumbers = ({
     const sleep = (ms) => {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
-
-    const revealNumber = (stage) => {
+	
+    const revealNumber = (stage, sum) => {
 		const index = sortedNumberArray[stage][0];
+		const newMultiplier = playerWinningsTier.find(tier => tier.threshold >= sum)?.multiplier || 0;
+		setMultiplier(newMultiplier);
 
         if (index === 0) {
             setShowFirstNumber(true);
@@ -190,59 +216,54 @@ export const ResultingNumbers = ({
         }
     }
 
+
 	// hooks reveal one number every 700ms
     useEffect(async () => {
         if (active) {
-			const stage = 0;
-			revealNumber(stage);
+			let stage = 0;
+			let sum = 0;
+			sum += sortedNumberArray[stage][1];
+			revealNumber(stage, sum);
+			stage = 1;
+			await sleep(900);
+			sum += sortedNumberArray[stage][1];
+			revealNumber(stage, sum);
+			stage = 2;
+			await sleep(900);
+			sum += sortedNumberArray[stage][1];
+			revealNumber(stage, sum);    
+			stage = 3;
+			await sleep(900);
+			sum += sortedNumberArray[stage][1];
+			revealNumber(stage, sum);
         }
     }, [active])
 
-    useEffect(async () => {
-        if (active) {
-            const stage = 1;
-            await sleep(700*stage);
-            revealNumber(stage);
-        }
-    }, [active])
-
-    useEffect(async () => {
-        if (active) {
-            const stage = 2;
-            await sleep(700*stage);
-            revealNumber(stage);
-        }
-    }, [active])
-
-    useEffect(async () => {
-        if (active) {
-            const stage = 3;
-            await sleep(700*stage);
-            revealNumber(stage);
-        }
-    }, [active])
-
-    
-    return (
-        <RandomNumbersCtn>        
-                <LeftEnd>
-                    <LeftCutOut color={background}/>                            
-                    <ArrowIcon src={WhiteLeftArrowSvg} />
-                </LeftEnd>
-                <VerticalDivider />
-                <Number blurred={!showFirstNumber}>{numberArray[0]}</Number>
-                <VerticalDivider />
-                <Number blurred={!showSecondNumber}>{numberArray[1]}</Number>
-                <VerticalDivider />
-                <Number blurred={!showThirdNumber}>{numberArray[2]}</Number>
-                <VerticalDivider />
-                <Number blurred={!showFourthNumber}>{numberArray[3]}</Number>            
-                <VerticalDivider />            
-                <RightEnd>
-                    <RightCutOut color={background}/>
-                    <ArrowIcon src={WhiteRightArrowSvg} />
-                </RightEnd>
-        </RandomNumbersCtn>
+      return (
+		<>
+			{active && 
+				<Multiplier>x{multiplier}</Multiplier>
+			}
+			<RandomNumbersCtn>        
+					<LeftEnd>
+						<LeftCutOut color={background}/>                            
+						<ArrowIcon src={WhiteLeftArrowSvg} />
+					</LeftEnd>
+					<VerticalDivider />
+					<Number blurred={!showFirstNumber}>{numberArray[0]}</Number>
+					<VerticalDivider />
+					<Number blurred={!showSecondNumber}>{numberArray[1]}</Number>
+					<VerticalDivider />
+					<Number blurred={!showThirdNumber}>{numberArray[2]}</Number>
+					<VerticalDivider />
+					<Number blurred={!showFourthNumber}>{numberArray[3]}</Number>            
+					<VerticalDivider />            
+					<RightEnd>
+						<RightCutOut color={background}/>
+						<ArrowIcon src={WhiteRightArrowSvg} />
+					</RightEnd>
+			</RandomNumbersCtn>		
+		</>
     )
 }
 
