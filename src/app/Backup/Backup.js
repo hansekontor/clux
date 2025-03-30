4e19a3af0182e69ae5993fe4a3d87f7278b7a1d3
Ryan Allen <ryansallen53@gmail.com>
2025-03-30 22:46:28 +0100

    chore: Move styled components to a separate file and update imports in Backup component

--- src/app/Backup/Backup.js
+++ src/app/Backup/Backup.js
@@ -1,13 +1,11 @@
 // node modules
 import React, { useState, useContext, useEffect } from 'react';
-import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 
 // custom react components
 import PrimaryButton, { SecondaryButton } from '@components/PrimaryButton';
 import SeedPhrase from '@components/SeedPhrase';
 import { WalletContext } from '@utils/context';
-import { FadeInAnimation } from '@components/CssAnimations';
 import FadeInOut from '@components/FadeInOut'
 import { successNotification } from '@components/Notifications';
 import { LargeHeading } from '@components/Text';
@@ -17,36 +15,7 @@ import { Paragraph } from '@components/Text';
 import CopyboardSvg from '@assets/svgs/copyboard.svg';
 
 // styled components
-const Modal = styled.div`
-    width: 95%;
-    background-color: #FEFFFE;
-    gap: 12px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 24px 0;
-    border-radius: 20px;
-    div, p {
-        text-align: center;
-    }
-`;
-
-const ModalCtn = styled.div`
-    width: 100%;
-    gap: 24px;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-
-    ${FadeInAnimation}
-`;
-const CopyboardIcon = styled.img`
-    position: relative;
-    top: 3px;
-    margin-right: 5px;
-`;
+import { CopyboardIcon, Modal, ModalCtn } from './components/Styled';
 
 
 const Backup = ({
@@ -79,7 +48,7 @@ const Backup = ({
         <>
             <FadeInOut show={!fadeOut} duration={300}>
                 <ModalCtn>
-                    <Modal >
+                    <Modal>
                         <LargeHeading>Backup Account</LargeHeading>
                         <Paragraph>This unhosted and non-custodial wallet associated with this lottery game is not backed up. Please backup your seed phrase and secure this wallet to avoid loss of funds from potential winnings.</Paragraph>
                         <SeedPhrase 
