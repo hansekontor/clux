6d9873642ffda0c2fe3dc03d9d70d9b772af6dfe
Ryan Allen <ryansallen53@gmail.com>
2025-04-05 21:41:14 +0100

    fix: add Subscript component import to Kyc

--- src/app/Checkout/Kyc/index.js
+++ src/app/Checkout/Kyc/index.js
@@ -14,6 +14,7 @@ import ContentHeader from './components/ContentHeader';
 import Subtitle from './components/Subtitle';
 import { BottomItem, TopItem } from './components/Item';
 import Content from './components/Content';
+import Subscript from './components/Subscript';
 
 export default function Kyc() {
     const { handleKYC } = useCheckout();
