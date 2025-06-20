740afa1aa4b25581622b648ecb9bee786b888f4c
Ryan Allen <ryansallen53@gmail.com>
2025-06-20 19:09:50 +0100

    chore: clean and update main app index

--- src/app/index.js
+++ src/app/index.js
@@ -1,48 +1,49 @@
 // node modules
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 import { CashoutProvider } from "blocklotto-sdk";
 
 import Layout from "@components/Layout";
-import Loading from "@components/Loading";
 import Tickets from "./Tickets";
 import Cashout from "./Cashout";
 import WaitingRoom from "./WaitingRoom";
 import Result from "./Result";
-
-// react components
-const Select = lazy(() => import("./Select"));
-const NotFound = lazy(() => import("./NotFound"));
+import Wallet from "./Wallet";
+import Affiliate from "./Affiliate";
+import Select from "./Home";
+import NotFound from "./NotFound";
 
 const App = () => {
-  const codeSplitLoader = <Loading>Loading</Loading>;
-
   return (
     <Layout>
-      <Suspense fallback={codeSplitLoader}>
-        <Switch>
-          <Route path="/select">
-            <Select />
-          </Route>
-          <Route path="/tickets">
-            <Tickets />
-          </Route>
-          <Route path="/waitingroom">
-            <WaitingRoom />
-          </Route>
-          <Route path="/result">
-            <Result />
-          </Route>
-          <Route path="/cashout">
-            <CashoutProvider>
-              <Cashout />
-            </CashoutProvider>
-          </Route>
-          <Redirect exact from="/" to="/select" />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
+      <Switch>
+        <Route path="/home">
+          <Select />
+        </Route>
+        <Route path="/tickets">
+          <Tickets />
+        </Route>
+        <Route path="/waitingroom">
+          <WaitingRoom />
+        </Route>
+        <Route path="/result">
+          <Result />
+        </Route>
+        <Route path="/wallet">
+          <Wallet />
+        </Route>
+        <Route path="/affiliate">
+          <Affiliate />
+        </Route>
+        <Route path="/cashout">
+          <CashoutProvider>
+            <Cashout />
+          </CashoutProvider>
+        </Route>
+        <Redirect exact from="/" to="/home" />
+        <Route component={NotFound} />
+      </Switch>
     </Layout>
   );
 };
