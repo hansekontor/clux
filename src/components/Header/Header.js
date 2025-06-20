b0dc2f4b6e31fccf389ae92c6d51cdb1195b2805
Ryan Allen <ryansallen53@gmail.com>
2025-06-20 19:07:53 +0100

    feat: add TUSDT logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 // react components
 import { Flex } from "../Common";
@@ -8,11 +9,25 @@ import Menu from "../Menu";
 import { Logo } from "../Icons";
 
 export default function Header({ hideMenu }) {
+  const history = useHistory();
+
+  const handleLogoClick = () => {
+    history.push("/home");
+  };
+
   return (
     <Flex marginBottom={0} justifyContent="space-between" alignItems="center">
-      <Logo />
+      <Logo onClick={handleLogoClick} style={{ cursor: "pointer" }} />
 
-      <Flex direction={"column"} alignItems="end">
+      <Flex justifyContent="center" alignItems="center" gap={2}>
+        <a href="https://dollar.mp/" target="_blank" rel="noopener noreferrer">
+          <img
+            src="/lottomp.png"
+            alt="BlockLotto Logo"
+            style={{ height: "30px", width: "30px", objectFit: "contain" }}
+          />
+        </a>
+
         {!hideMenu && <Menu />}
       </Flex>
     </Flex>
