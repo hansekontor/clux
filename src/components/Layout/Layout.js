7f2d08c6fdd16826bb86cc3130b09ba2c6fee79c
Ryan Allen <ryansallen53@gmail.com>
2025-06-30 11:53:44 +0100

    refactor: Enhance Layout component structure with Flex and Container for improved responsiveness

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { StyledContainer, StyledWrapper } from './Layout.styles';
+import React from "react";
+import { StyledContainer, StyledWrapper } from "./Layout.styles";
+import { Flex, Container } from "@components/Common";
+import Header from "@components/Header";
 
 export default function Layout({ children }) {
-
-    return (
-        <StyledWrapper>
-            <StyledContainer>
-                {children}
-            </StyledContainer>
-        </StyledWrapper>
-    )
+  return (
+    <StyledWrapper>
+      <StyledContainer>
+        <Flex
+          direction={"column"}
+          minHeight={"100dvh"}
+          style={{ overflow: "hidden" }}
+        >
+          <Container
+            height="100dvh"
+            style={{
+              overflowY: "auto",
+            }}
+          >
+            <Flex
+              direction="column"
+              height="100%"
+              paddingTop={2}
+              paddingBottom={2}
+              gap={2}
+            >
+              <Header />
+              {children}
+            </Flex>
+          </Container>
+        </Flex>
+      </StyledContainer>
+    </StyledWrapper>
+  );
 }
