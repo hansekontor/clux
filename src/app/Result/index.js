959775d5d32274226f14c7db815b92d757831f68
Ryan Allen <ryansallen53@gmail.com>
2025-06-30 11:54:45 +0100

    refactor: Revamp Result component by consolidating ticket handling logic, improving error messaging, and enhancing layout with Flex components

--- src/app/Result/index.js
+++ src/app/Result/index.js
@@ -2,45 +2,65 @@ import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 // core functions
-import { useApp } from "blocklotto-sdk";
-import sleep from "../../utils/sleep";
+import { useApp, getWalletState } from "blocklotto-sdk";
+import useQuery from "../../hooks/useQuery";
 
 // react components
-import Header from "@components/Header";
-import { Container, Flex } from "@components/Common";
+import { Flex } from "@components/Common";
 import Button from "@components/Button";
 import Typography from "@components/Typography";
 import { TicketNumbers } from "../../components/Misc";
+import Alert from "@components/Alert";
 
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { Dollar } from "./components/Dollar";
 
 export default function Result() {
-  const [animation, setAnimation] = useState(true);
-  const { gameTickets, setGameTickets, ticketsToRedeem } = useApp();
   const history = useHistory();
-  const activeTicket = gameTickets[0];
-  const redemptionsOutstanding = ticketsToRedeem.length > 0;
+  const { wallet } = useApp();
+  const { tickets } = getWalletState(wallet);
+  const query = useQuery();
 
-  // redirect if ticket data missing
-  useEffect(() => {
-    if (!activeTicket) {
-      sleep(1000);
-      history.push("/home");
-    } else if (!activeTicket.parsed) {
-      sleep(1000);
-      history.push("/home");
-    }
-  }, []);
+  const [animation, setAnimation] = useState(true);
+
+  const ticketHash = query.get("ticket") || "";
+  const redeemAll = query.get("redeemAll") || "";
+
+  const currentTicket = tickets.find(
+    (ticket) => ticket.issueTx?.hash === ticketHash
+  );
+
+  console.log("currentTicket", currentTicket);
+
+  if (!currentTicket || !currentTicket.parsed) {
+    return (
+      <Flex direction="column" height="100%" justifyContent="center" gap={2}>
+        <Alert type="error">
+          No ticket found with the provided hash. Please ensure you have a valid
+          ticket.
+        </Alert>
+        <Button size="sm" color="tertiary" onClick={() => history.push("/")}>
+          Go Home
+        </Button>
+      </Flex>
+    );
+  }
+
+  // DOM variables
+  // account for strange browser behaviour and try both properties
+  const payoutAmountNum = currentTicket.parsed.payoutAmountNum;
+  const actualPayoutNum = currentTicket.parsed.actualPayoutNum;
+  const amount =
+    payoutAmountNum >= 0 ? payoutAmountNum / 100 : actualPayoutNum / 100;
+  const resultingNumbers = currentTicket.parsed?.resultingNumbers;
+  const buttonText = redeemAll ? "Redeem Next Ticket" : "Play Again";
+  const isWinner = amount > 0;
 
   // handlers
   const handleRedirect = () => {
-    setGameTickets([]);
-    if (ticketsToRedeem.length > 0) {
-      console.log("tickets available: go to waiting room");
-      history.push("/waitingroom");
+    if (redeemAll) {
+      history.push("/waitingroom?redeemAll=true");
     } else {
-      console.log("no more tickets: go to select");
       history.push("/home");
     }
   };
@@ -59,129 +79,84 @@ export default function Result() {
     setAnimation(false);
   };
 
-  // DOM variables
-  // account for strange browser behaviour and try both properties
-  const payoutAmountNum = activeTicket.parsed?.payoutAmountNum;
-  const actualPayoutNum = activeTicket.parsed?.actualPayoutNum;
-  const amount =
-    payoutAmountNum >= 0 ? payoutAmountNum / 100 : actualPayoutNum / 100;
-  const resultingNumbers = activeTicket.parsed?.resultingNumbers;
-  const buttonText = redemptionsOutstanding
-    ? "Redeem Next Ticket"
-    : "Play Again";
-  const isWinner = amount > 0;
-
   return (
     <Flex
-      direction={"column"}
-      minHeight={"100dvh"}
-      style={{ overflow: "hidden" }}
+      direction="column"
+      gap={2}
+      justifyContent="center"
+      alignItems="center"
+      height="100%"
     >
-      <Container
-        height="100dvh"
-        style={{
-          overflowY: "auto",
-        }}
-      >
+      {!animation ? (
         <Flex
           direction="column"
           height="100%"
-          paddingTop={2}
-          paddingBottom={2}
-          gap={2}
+          gap={4}
+          width="100%"
+          justifyContent="space-between"
         >
-          <Header />
-
           <Flex
-            direction="column"
-            gap={2}
+            height="100%"
+            width="100%"
             justifyContent="center"
             alignItems="center"
-            height="100%"
+            position="relative"
           >
-            {!animation ? (
-              <Flex
-                direction="column"
-                height="100%"
-                gap={4}
-                width="100%"
-                justifyContent="space-between"
-              >
-                <Flex
-                  height="100%"
-                  width="100%"
-                  justifyContent="center"
-                  alignItems="center"
-                  position="relative"
+            <DotLottieReact
+              src={`animations/${isWinner ? "confetti" : "smoke"}.lottie`}
+              autoplay
+              loop={isWinner}
+            />
+            <Flex position="absolute" top={0} left={0} right={0} bottom={0}>
+              {isWinner ? (
+                <Dollar>
+                  <Typography variant="h2" fontWeight="400" fontSize="5rem">
+                    ${amount}
+                  </Typography>
+                </Dollar>
+              ) : (
+                <Typography variant="h2" fontWeight="400" fontSize="5rem">
+                  ${amount}
+                </Typography>
+              )}
+            </Flex>
+          </Flex>
+          <Flex gap={2} direction="column" width="100%">
+            <Flex gap={1} direction="column" width="100%">
+              <Flex gap={1} width="100%">
+                <Button
+                  fullWidth
+                  size="sm"
+                  color="tertiary"
+                  onClick={handleCashoutRedirect}
                 >
-                  <DotLottieReact
-                    src={`animations/${isWinner ? "confetti" : "smoke"}.lottie`}
-                    autoplay
-                    loop={isWinner}
-                  />
-                  <Flex
-                    position="absolute"
-                    top={0}
-                    left={0}
-                    right={0}
-                    bottom={0}
-                  >
-                    {isWinner ? (
-                      <Dollar>
-                        <Typography
-                          variant="h2"
-                          fontWeight="400"
-                          fontSize="5rem"
-                        >
-                          ${amount}
-                        </Typography>
-                      </Dollar>
-                    ) : (
-                      <Typography variant="h2" fontWeight="400" fontSize="5rem">
-                        ${amount}
-                      </Typography>
-                    )}
-                  </Flex>
-                </Flex>
-                <Flex gap={2} direction="column" width="100%">
-                  <Flex gap={1} direction="column" width="100%">
-                    <Flex gap={1} width="100%">
-                      <Button
-                        fullWidth
-                        size="sm"
-                        color="tertiary"
-                        onClick={handleCashoutRedirect}
-                      >
-                        Cashout
-                      </Button>
-                      <Button
-                        fullWidth
-                        size="sm"
-                        color="tertiary"
-                        onClick={handleTicketsRedirect}
-                      >
-                        Tickets
-                      </Button>
-                    </Flex>
-                    <TicketNumbers numbers={resultingNumbers} />
-                    <Button onClick={handleRedirect}>{buttonText}</Button>
-                  </Flex>
-                </Flex>
+                  Cashout
+                </Button>
+                <Button
+                  fullWidth
+                  size="sm"
+                  color="tertiary"
+                  onClick={handleTicketsRedirect}
+                >
+                  Tickets
+                </Button>
               </Flex>
-            ) : (
-              <DotLottieReact
-                src="animations/ticket.lottie"
-                autoplay
-                dotLottieRefCallback={(dotLottie) => {
-                  if (dotLottie) {
-                    dotLottie.addEventListener("complete", handleFinishAnimation);
-                  }
-                }}
-              />
-            )}
+              <TicketNumbers numbers={resultingNumbers} />
+              <Button onClick={handleRedirect}>{buttonText}</Button>
+            </Flex>
           </Flex>
         </Flex>
-      </Container>
+      ) : (
+        <DotLottieReact
+          src="animations/ticket.lottie"
+          autoplay
+          dotLottieRefCallback={(dotLottie) => {
+            if (dotLottie) {
+              dotLottie.addEventListener("complete", handleFinishAnimation);
+            }
+          }}
+        />
+      )}
     </Flex>
   );
 }
